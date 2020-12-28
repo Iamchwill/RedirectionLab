542b18d9c3422a211a051339187adc08db2a408e
Iamchwill <wchen20@stuy.edu>
2020-12-28 01:58:18 -0500

    Made MakeStars.java

---
+++ MakeStars.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MakeStars{
+  public static void main(String[] args){
+    Scanner line = new Scanner(System.in);
+    while(line.hasNext()){
+      Scanner word = new Scanner(line.nextLine());
+      while(word.hasNext()){
+        String stars = word.next();
+        for(int i = 0; i < stars.length(); i++) System.out.print("*");
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+}
